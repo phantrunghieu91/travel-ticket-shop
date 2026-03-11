3ebbe253c351b2f28051feb89cd1e1beffe58b89
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-03-11 15:51:42 +0700

    UPDATE: - Added accommodation type selection to product edit page in admin - Added star rating selection to product edit page in admin

--- inc/AdminProductPageController.php
+++ inc/AdminProductPageController.php
@@ -6,15 +6,26 @@
 class AdminProductPageController {
   private $highlights = [];
   private $amenities = [];
+  private $star_ratings = [];
+  private $accommodation_types = [];
   public function __construct() {
     $this->set_custom_fields();
     add_filter( 'woocommerce_product_data_tabs', [ $this, 'add_data_tabs' ] );
     add_action( 'woocommerce_product_data_panels', [ $this, 'add_data_panel_content' ] );
+    add_action( 'woocommerce_product_options_general_product_data', [ $this, 'add_general_product_data_fields' ] );
     add_action( 'woocommerce_process_product_meta', [ $this, 'save_custom_fields' ] );
   }
   private function set_custom_fields() {
     $highlight_data = get_field( 'highlights', 'gpw_product_data');
     $amenities_data = get_field( 'amenities', 'gpw_product_data');
+    $this->star_ratings = get_terms( [
+      'taxonomy' => 'star-rating',
+      'hide_empty' => false,
+    ] );
+    $this->accommodation_types = get_terms( [
+      'taxonomy' => 'accommodation-type',
+      'hide_empty' => false,
+    ] );
     if( empty( $highlight_data ) ) {
       return;
     }
@@ -113,6 +124,36 @@ class AdminProductPageController {
     </style>
     <?php 
   }
+  public function add_general_product_data_fields() {
+    $selected_accommodation_type = get_the_terms( get_the_ID(), 'accommodation-type' );
+    $selected_accommodation_type_id = $selected_accommodation_type && ! is_wp_error($selected_accommodation_type) ? reset( $selected_accommodation_type )->term_id : '';
+    $selected_star_rating = get_the_terms( get_the_ID(), 'star-rating' );
+    $selected_star_rating_id = $selected_star_rating && ! is_wp_error($selected_star_rating) ? reset( $selected_star_rating )->term_id : '';
+    woocommerce_wp_select([
+      'id' => 'gpw_accommodation_type',
+      'name' => 'gpw_accommodation_type',
+      'label' => __('Loại hình lưu trú', 'gpw'),
+      'desc_tip' => 'true',
+      'description' => __('Chọn loại hình lưu trú của sản phẩm', 'gpw'),
+      'options' => array_reduce( $this->accommodation_types, function( $options, $type ) {
+        $options[ $type->term_id ] = $type->name;
+        return $options;
+      }, [ '' => __('Chọn loại hình lưu trú', 'gpw') ] ),
+      'value' => $selected_accommodation_type_id,
+    ]);
+    woocommerce_wp_select([
+      'id' => 'gpw_star_rating',
+      'name' => 'gpw_star_rating',
+      'label' => __('Xếp hạng sao', 'gpw'),
+      'desc_tip' => 'true',
+      'description' => __('Chọn xếp hạng sao của khách sạn', 'gpw'),
+      'options' => array_reduce( $this->star_ratings, function( $options, $type ) {
+        $options[ $type->term_id ] = $type->name;
+        return $options;
+      }, ['' => __('Chọn xếp hạng sao', 'gpw')] ),
+      'value' => $selected_star_rating_id,
+    ]);
+  }
   public function save_custom_fields( $post_id ) {
     if( isset( $_POST['gpw_highlight'] ) ) {
       $highlights = array_map( 'intval', $_POST['gpw_highlight'] );
@@ -121,6 +162,14 @@ class AdminProductPageController {
     if( isset( $_POST['gpw_amenities'] ) ) {
       $amenities = array_map( 'intval', $_POST['gpw_amenities'] );
       update_post_meta( $post_id, '_gpw_amenities', $amenities );
+    }
+    if( isset( $_POST['gpw_accommodation_type'] ) ) {
+      $accommodation_type_id = intval( $_POST['gpw_accommodation_type'] );
+      wp_set_post_terms( $post_id, $accommodation_type_id, 'accommodation-type' );
+    }
+    if( isset( $_POST['gpw_star_rating'] ) ) {
+      $star_rating_id = intval( $_POST['gpw_star_rating'] );
+      wp_set_post_terms( $post_id, $star_rating_id, 'star-rating' );
     }
   }
 }
