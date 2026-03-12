5ce89f771cd5ba6e35c75676876ddb9f42e9b2dc
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-03-12 17:29:09 +0700

    UPDATE: company_info shortcode - add class

--- shortcodes/company_info.php
+++ shortcodes/company_info.php
@@ -1,20 +1,23 @@
 <?php
 add_shortcode('company_info', function($atts){
-  extract(shortcode_atts(array(
-    'type' => '') , $atts));
+  extract(shortcode_atts([
+    'type' => 'address',
+    'class' => '',
+  ], $atts));
   switch($type){
     case 'address':
-      return get_field('address', 'company_info');
+      $address = get_field('address', 'company_info');
+      return sprintf('<span class="%s">%s</span>', esc_attr($class), esc_html($address));
     case 'phone':
       $phone = get_field('phone_number', 'company_info');
       // replace all non-numeric characters
       $phone_link = preg_replace('/\D/', '', $phone);
-      return '<a href="tel:' . $phone_link . '">' . esc_html($phone) . '</a>';
+      return sprintf('<a class="%s" href="tel:%s">%s</a>', esc_attr($class), $phone_link, esc_html($phone));
     case 'email':
       $email = get_field('email', 'company_info');
       // check if email is valid
       if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-        return '<a href="mailto:' . $email . '">' . esc_html($email) . '</a>';
+        return sprintf('<a class="%s" href="mailto:%s">%s</a>', esc_attr($class), $email, esc_html($email));
       } else {
         return 'Invalid email';
       }
