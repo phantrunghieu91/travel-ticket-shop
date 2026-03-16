d7ec40b54a6b109f8a19cd62ca4d1789a9a2c21f
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-03-16 15:46:41 +0700

    UPDATE: Footer - Added expand/collapse functionality to footer menu on mobile view.

--- js/theme-init.js
+++ js/theme-init.js
@@ -42,4 +42,42 @@ document.addEventListener('DOMContentLoaded', function() {
       } );
     }
   }.init();
+  // Footer
+  const footerMenuToggle = {
+    init() {
+      try {
+        this.cacheElements();
+        this.initObserver();
+      } catch (error) {
+        console.warn('FOOTER MENU TOGGLE ERROR: ', error);
+      }
+    },
+    cacheElements() {
+      this.menus = [...document.querySelectorAll('.footer__menu')];
+      if( !this.menus.length ) throw new Error('No footer menus found');
+    },
+    handleToggle( event, menu ) {
+      const target = event.target;
+      const isExpanded = menu.classList.contains('footer__menu--expanded');
+      if( target.tagName === 'H3' ) {
+        menu.classList.toggle('footer__menu--expanded', !isExpanded );
+      }
+    },
+    initObserver() {
+      const menuHandlers = this.menus.map( menu => event => this.handleToggle(event, menu) );
+      const observer = new ResizeObserver( entries => {
+        const entry = entries[0];
+        if( entry.contentRect.width < 850 ) {
+          this.menus.forEach( (menu, index) => {
+            menu.addEventListener('click', menuHandlers[index] );
+          } );
+        } else {
+          this.menus.forEach( (menu, index) => {
+            menu.removeEventListener('click', menuHandlers[index] );
+          } );
+        }
+      } );
+      observer.observe(document.body);
+    }
+  }.init();
 });
