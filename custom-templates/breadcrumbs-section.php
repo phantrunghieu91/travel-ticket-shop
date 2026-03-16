d67097f9ff6b6eb7f402ea755cf7d85bd618e950
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-03-16 09:18:16 +0700

    UPDATE: Create breadcrumbs template, added breadcrumbs for single product + style

---
+++ custom-templates/breadcrumbs-section.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template: Global - Breadcrumbs section
+ */
+if( !function_exists( 'rank_math_the_breadcrumbs' ) ) {
+  return;
+};
+?>
+<section class="gpw-breadcrumbs">
+  <div class="section__inner">
+    <?php rank_math_the_breadcrumbs(); ?>
+  </div>
+</section>
--- single-product.php
+++ single-product.php
@@ -8,6 +8,8 @@ get_header();
 ?>
 <div id="content">
 
+  <?php get_template_part( 'custom-templates/breadcrumbs-section' ) ?>
+
   <?php get_template_part( 'custom-templates/woocommerce/single-product/header' ) ?>
 
   <?php get_template_part( 'custom-templates/woocommerce/single-product/gallery-section' ) ?>
